d65de8780136df45d116731596a57cc3ecfe1ebe
Sridhar Madala <Si.madala19@gmail.com>
2018-11-16 17:28:58 -0800

    completed getting list data from servr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=c918_demouser';
+
+export async function getListData(){
+
+    const resp = await axios.get(BASE_URL + API_KEY);
+
+    return {
+        type: types.GET_ALL_LIST_DATA,
+        payload: resp,
+    }
+}
